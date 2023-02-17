0edc3dd5b884bcd5f9853cfbdb230d7697b66b7c
matcha-scoft <79491716+matcha-scoft@users.noreply.github.com>
2023-02-17 13:58:21 +0800

    Add files via upload

---
+++ KP-ABE/src/main/java/BC/ABE/entity/Pk.java
@@ -0,0 +1,23 @@
+package BC.ABE.entity;
+
+import it.unisa.dia.gas.jpbc.Element;
+import it.unisa.dia.gas.jpbc.Pairing;
+
+import java.util.ArrayList;
+
+public class Pk {
+
+    public Pairing p;
+
+    public Integer d = 4;
+
+    public Element g;
+
+    public Element g1;
+
+    public Element g2;
+
+    public Element h;
+
+    // public ArrayList<Element> attrs;
+}
