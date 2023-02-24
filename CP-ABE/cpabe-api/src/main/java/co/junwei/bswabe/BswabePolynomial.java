0f2cb2656ede000b0324506582b4942704044259
phrara <95560367+phrara@users.noreply.github.com>
2023-02-24 15:01:47 +0800

    Delete CP-ABE/cpabe-api directory

--- CP-ABE/cpabe-api/src/main/java/co/junwei/bswabe/BswabePolynomial.java
+++
@@ -1,9 +0,0 @@
-package co.junwei.bswabe;
-
-import it.unisa.dia.gas.jpbc.Element;
-
-public class BswabePolynomial {
-	int deg;
-	/* coefficients from [0] x^0 to [deg] x^deg */
-	Element[] coef; /* G_T (of length deg+1) */
-}
